bd808280f912824a716ba76ddd49706407a53032
Michael Wayne <Kromwell005@gmail.com>
2024-05-15 09:38:04 +0300

    Added match cases

--- rs_cargo_projects/guessing_game/src/main.rs
+++ rs_cargo_projects/guessing_game/src/main.rs
@@ -1,3 +1,43 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input the number");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+
+
+        if guess.contains("quit") {
+            println!("Quitting...\nGame over!");
+            break;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
